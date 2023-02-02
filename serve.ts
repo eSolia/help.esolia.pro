c123f2c335bf4dc10368f9d27df098a21e351b6c
Rick Cogley <rick.cogley@esolia.co.jp>
2023-02-02 10:35:38 +0000

    simplify serve.ts to basic

--- serve.ts
+++ serve.ts
@@ -1,26 +1,10 @@
 import Server from "https:/deno.land/x/lume/core/server.ts";
-import csp from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/csp/mod.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_dist`,
 });
 
-// Use the middleware and pass your preferred options:
-server.use(csp({
-  "Strict-Transport-Security": {
-	maxAge: 63072000,
-	includeSubDomains: true,
-	preload: true,
-  },
-  "Referrer-Policy": ["no-referrer", "strict-origin-when-cross-origin"],
-  "X-Frame-Options": true,
-  "X-Content-Type-Options": true,
-  "X-XSS-Protection": true,
-  "X-Permitted-Cross-Domain-Policies": true,
-  "X-Powered-By": false,
-}),
+server.start();
 
-server.start(),
-
-console.log("Listening on http://localhost:8000"))
+console.log("Listening on http://localhost:8000");
