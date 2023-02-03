03631ee6ac4698f72e89007c5344951a0621f42c
Rick Cogley <rick.cogley@esolia.co.jp>
2023-02-03 02:15:10 +0000

    add some middlewares esp for 404

--- serve.ts
+++ serve.ts
@@ -1,10 +1,18 @@
 import Server from "https:/deno.land/x/lume/core/server.ts";
+import expires from "https:/deno.land/x/lume/middlewares/expires.ts";
+import notfound from "https:/deno.land/x/lume/middlewares/not_found.ts";
+import www from "https://deno.land/x/lume/middlewares/www.ts"
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_dist`,
 });
 
+server.use(expires());
+server.use(notfound());
+server.use(www({
+  add: false, // false to remove, true to add it.
+}));
 server.start();
 
 console.log("Listening on http://localhost:8000");
