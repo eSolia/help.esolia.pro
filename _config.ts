454e78aa8303f1f5fa9716f5d46b18cbc4894d63
Rick Cogley <rick.cogley@esolia.co.jp>
2023-12-29 17:50:10 +0900

    Prep for netlify

--- _config.ts
+++ _config.ts
@@ -7,4 +7,11 @@ const site = lume({
 site.use(notes());
 site.copy("assets");
 
+// Create zip and tree scripts
+site.script("zipsite", "zip -r _site/help_esolia_pro_site.zip _site");
+site.script("maketree", "cd _site && tree -H . -L 5 --charset utf-8 -C -h -o esolia_help_tree.html");
+// Execute scripts after build
+site.addEventListener("afterBuild", "zipsite");
+site.addEventListener("afterBuild", "maketree");
+
 export default site;
