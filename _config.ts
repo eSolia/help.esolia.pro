717f4a31fbec082367a993bbd73bcb18de549b84
Rick Cogley <rick.cogley@esolia.co.jp>
2024-10-24 05:32:07 +0900

    try adding plugins

--- _config.ts
+++ _config.ts
@@ -1,6 +1,18 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
 
+//add to base from wiki
+import date from "lume/plugins/date.ts"; //
+import { enUS } from "npm:date-fns/locale/en-US";
+import { ja } from "npm:date-fns/locale/ja";
+import metas from "lume/plugins/metas.ts"; 
+import sitemap from "lume/plugins/sitemap.ts";
+import attributes from "lume/plugins/attributes.ts";
+import filterPages from "lume/plugins/filter_pages.ts";
+import sri from "lume/plugins/sri.ts";
+import autoTrim from "vento/plugins/auto_trim.ts";
+import terser from "lume/plugins/terser.ts";
+
 const site = lume({
   src: "./src",
   location: new URL("https://help.esolia.pro"),
@@ -9,6 +21,19 @@ site.use(wiki({
   languages: ["ja", "en"],
   defaultLanguage: "ja",
 }));
+site.use(date(
+  {
+    locales: { enUS, ja },
+  }
+))
+site.use(metas());
+site.use(sitemap());
+site.use(attributes());
+site.use(filterPages());
+site.use(sri());
+site.use(autoTrim());
+site.use(terser());
+
 site.copy("assets");
 site.copy("menu.js");
 
